b7db681e25aa0e668e66bebc87381b6d8fa8e980
MariaLFioretti <marialuizafiorettiribet@gmail.com>
2024-11-22 12:13:20 -0300

    added new motivational phrases
    new functions

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-var imgAtual = "img/imagem2.jpg";
-var imgAnterior = "img/imagem1.png";
+const btnMensagem = document.getElementById("btnMensagem");
 
-function trocar(){
-    document.getElementById("figura").src = imgAtual;
-    let aux = imgAtual;
-    imgAtual = imgAnterior;
-    imgAnterior = aux;
+btnMensagem.addEventListener('click', opcoesSetembro);
+
+function opcoesSetembro()
+{
+    console.log ('vou escrever uma mensagem no <body>');
 }
+
+
+
+var frases= [
+  'A vida é preciosa e cada dia é uma nova oportunidade para recomeçar.',
+  'você não está sozinho, sempre a alguém disposto a ouvir e ajudar.',
+  'As tempestades da vida são temporárias, mantenha a esperança acesa.',
+  'Seus sentimentos são válidos. Não tenha medo de espressá-los.',
+  'Cada pequeno passo conta, celebre suas vitórias por menor que sejam.',
+  'A escuridão pode ser profunda, mas sempre há uma luz esperando para brilhar.',
+  'Falar sobre seus problemas é um sinal de força, não de fraqueza.',
+  'Acredite em você e na sua capacidade de superar desafios.',
+  'A vida é um presente, cuide dela com carinho e atenção.',
+  'Seu sorriso pode ser a luz na vida de alguém, espalhe positividade!',
+  'Lembre-se: pedir ajuda é um ato de coragem e não de fraqueza.',
+  'Conecte-se com quem ama, relacionamentos saudáveis são fundamentais para o bem-estar.',
+  'O amanha pode ser melhor, mantenha a fé e a esperança em seu coração.',
+  'Você é importante, e sua história merece ser contada.',
+]
+
+
+
+
+
+
+
 
 
 
